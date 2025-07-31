149e6b803e742dffd87d26dfb026d5849c19b473
Ronish Shrestha <157086816+RonishS@users.noreply.github.com>
2025-07-31 23:35:59 +0545

    Update cart_page.py

--- pages/cart_page.py
+++ pages/cart_page.py
@@ -1,1 +1,12 @@
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
 
+class CartPage(BasePage):
+    CHECKOUT_BUTTON = (By.LINK_TEXT, "Checkout")
+    CART_ITEMS = (By.XPATH, "//div[@class='table-responsive']//tbody/tr")
+    
+    def proceed_to_checkout(self):
+        self.click(self.CHECKOUT_BUTTON)
+    
+    def get_cart_item_count(self):
+        return len(self.driver.find_elements(*self.CART_ITEMS))
