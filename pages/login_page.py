1d322db57681ff2aea6ea05df4a36552b8904c4c
Ronish Shrestha <157086816+RonishS@users.noreply.github.com>
2025-07-31 23:35:15 +0545

    Update login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,1 +1,15 @@
+from selenium.webdriver.common.by import By
+from .base_page import BasePage
 
+class LoginPage(BasePage):
+    EMAIL_FIELD = (By.ID, "input-email")
+    PASSWORD_FIELD = (By.ID, "input-password")
+    LOGIN_BUTTON = (By.XPATH, "//input[@value='Login']")
+    
+    def open_login_page(self):
+        self.driver.get("https://demo.opencart.com/index.php?route=account/login")
+    
+    def login(self, email, password):
+        self.enter_text(self.EMAIL_FIELD, email)
+        self.enter_text(self.PASSWORD_FIELD, password)
+        self.click(self.LOGIN_BUTTON)
