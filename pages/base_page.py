044057319924b259735d9660678c181332db540d
Ronish Shrestha <157086816+RonishS@users.noreply.github.com>
2025-07-31 23:34:47 +0545

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,1 +1,23 @@
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
+class BasePage:
+    def __init__(self, driver):
+        self.driver = driver
+        self.wait = WebDriverWait(driver, 10)
+    
+    def click(self, locator):
+        element = self.wait.until(EC.element_to_be_clickable(locator))
+        element.click()
+    
+    def enter_text(self, locator, text):
+        element = self.wait.until(EC.visibility_of_element_located(locator))
+        element.clear()
+        element.send_keys(text)
+    
+    def is_visible(self, locator):
+        try:
+            return self.wait.until(EC.visibility_of_element_located(locator)).is_displayed()
+        except TimeoutException:
+            return False
